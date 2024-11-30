f4173ce8113aba8a5d654427cc8406f4e87f182a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-30 19:51:23 +0300

    Update any_13.cpp

--- any_13.cpp
+++ any_13.cpp
@@ -10,16 +10,16 @@ int main()
 
     any a{ "Kaya"s };
 
-    auto& ra = std::any_cast<string&>(a);
+    auto& ra = any_cast<string&>(a);
     ra[0] = 'M';
 
-    std::cout << any_cast<string const&>(a) << '\n'; 
+    cout << any_cast<string const&>(a) << '\n';
 
-    auto str = any_cast<string&&>(move(a)); 
+    auto str = any_cast<string&&>(move(a));
 
     static_assert(is_same_v<decltype(str), string>);
 
-    std::cout << "a.size(): " << any_cast<string>(&a)->size() << '\n';
+    cout << "a.size(): " << any_cast<string>(&a)->size() << '\n';
 
     cout << "str: " << str << '\n';
 }
