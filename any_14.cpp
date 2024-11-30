cefb894aaf0b8ca404418537b8a58519df98c0d4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-30 19:52:55 +0300

    Update any_14.cpp

--- any_14.cpp
+++ any_14.cpp
@@ -6,13 +6,11 @@ struct Der : Base { /*....*/ };
 
 int main()
 {
-	using namespace std;
-
-	any a = Der();
+	std::any a = Der();
 	if (Base* base_ptr = std::any_cast<Base>(&a)) {
-		cout << "correct type\n";
+		std::cout << "correct type\n";
 	}
 	else {
-		cout << "incorrect type\n";
+		std::cout << "incorrect type\n";
 	}
 }
