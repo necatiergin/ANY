b98338e4a027868e94b0b4626e575f6e759c4f6e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-30 19:50:13 +0300

    Update any_11.cpp

--- any_11.cpp
+++ any_11.cpp
@@ -13,15 +13,13 @@ public:
 
 int main()
 {
-	using namespace std;
-
 	auto fcmp = [](int x, int y) {return abs(x) < abs(y); };
-	any a;
+	std::any a;
 
 	a = 12; //int
 	a = Nec{ 10, 20 }; //Nec
 	a.emplace<Nec>(2, 5); //Nec
-	a.emplace<string>(10, 'A'); //string
-	a.emplace<vector<int>>(100); //vector<int>
-	a.emplace<set<int, decltype(fcmp)>>({ 1, 5, -4, -6, 3 }, fcmp);
+	a.emplace<std::string>(10, 'A'); //string
+	a.emplace<std::vector<int>>(100); //vector<int>
+	a.emplace<std::set<int, decltype(fcmp)>>({ 1, 5, -4, -6, 3 }, fcmp);
 }
